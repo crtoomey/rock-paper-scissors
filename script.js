58b29a393549eed3f440a947db6d29669669446d
Colin Toomey <86315504+crtoomey@users.noreply.github.com>
2024-10-22 14:18:43 -0400

    Modify index.html and script.js
    Added some more UI to the game in index.html so that users can see the result of the round (win, loss, or tie) and the result of the game (win or loss).
    Added functionality to script.js to display the result of each round and to the game and then display those on the page.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let humanScore = 0;
 let computerScore = 0;
 let round = 0;
-document.getElementById("human-score").innerHTML = humanScore.toString();
-document.getElementById("computer-score").innerHTML = computerScore.toString();
-document.getElementById("round-counter").innerHTML = round.toString();
+document.getElementById("human-score").innerText = humanScore.toString();
+document.getElementById("computer-score").innerText = computerScore.toString();
+document.getElementById("round-counter").innerText = round.toString();
 
 // Gets the computer choice randomly and displays choice in span
 function getComputerChoice() {
@@ -19,7 +19,7 @@ function getComputerChoice() {
     } else {
         comChoice = 'something went wrong';
     }
-    document.getElementById("computer-choice").innerHTML = comChoice;
+    document.getElementById("computer-choice").innerText = "The computer chose: " + comChoice;
     return comChoice;
 }
 
@@ -28,7 +28,7 @@ function getComputerChoice() {
 // Gets the human choice based on which button is clicked and displays choice in span
 function getHumanChoice(choice) {
     let humChoice = choice;
-    document.getElementById("human-choice").innerHTML = humChoice;
+    document.getElementById("human-choice").innerText = "You chose: " + humChoice;
     console.log(choice);
     let comChoice = getComputerChoice();
     playGame(humChoice, comChoice);
@@ -40,34 +40,39 @@ function playRound(humanChoice, computerChoice) {
         Number(round);
         humanScore++;
         round++;
-        document.getElementById("human-score").innerHTML = humanScore.toString();
-        document.getElementById("round-counter").innerHTML = round.toString();
+        document.getElementById("human-score").innerText = humanScore.toString();
+        document.getElementById("round-counter").innerText = round.toString();
+        document.getElementById("round-result").innerText = "You won Round " + round.toString();
         console.log(humanScore, round, computerScore);
     } else if (humanChoice === 'paper' && computerChoice === 'rock') {
         Number(humanScore);
         Number(round);
         humanScore++;
         round++;
-        document.getElementById("human-score").innerHTML = humanScore.toString();
-        document.getElementById("round-counter").innerHTML = round.toString();
+        document.getElementById("human-score").innerText = humanScore.toString();
+        document.getElementById("round-counter").innerText = round.toString();
+        document.getElementById("round-result").innerText = "You won Round " + round.toString();
         console.log(humanScore, round, computerScore);
     } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
         Number(humanScore);
         Number(round);
         humanScore++;
         round++;
-        document.getElementById("human-score").innerHTML = humanScore.toString();
-        document.getElementById("round-counter").innerHTML = round.toString();
+        document.getElementById("human-score").innerText = humanScore.toString();
+        document.getElementById("round-counter").innerText = round.toString();
+        document.getElementById("round-result").innerText = "You won Round " + round.toString();
         console.log(humanScore, round, computerScore);
     } else if (humanChoice === computerChoice) {
+        document.getElementById("round-result").innerText = "Tied! Go again!";
         console.log('tied');
     } else {
         Number(computerScore);
         Number(round);
         computerScore++;
         round++;
-        document.getElementById("computer-score").innerHTML = computerScore.toString();
-        document.getElementById("round-counter").innerHTML = round.toString();
+        document.getElementById("computer-score").innerText = computerScore.toString();
+        document.getElementById("round-counter").innerText = round.toString();
+        document.getElementById("round-result").innerText = "You lost Round " + round.toString();
         console.log(humanScore, round, computerScore);
     }
     return Number(round), Number(humanScore), Number(computerScore);
@@ -80,16 +85,16 @@ function playGame(humanChoice, computerChoice) {
     let restartButton = document.getElementById('restart-button');
 
     playRound(humanChoice, computerChoice);
-    if (round === 5) {
+    if (humanScore === 3 || computerScore === 3 || round === 5) {
         rockButton.style.display = 'none';
         paperButton.style.display = 'none';
         scissorsButton.style.display = 'none';
         restartButton.style.display = 'inline-block';
 
-        if (humanScore > computerScore) {
-            alert('Game Over! You won!');
-        } else {
-            alert('Game Over! You lost!')
+        if (humanScore === 3) {
+            document.getElementById("game-result").innerText = "You won the Game! Use the Restart button to restart!";
+        } else if (computerScore === 3) {
+            document.getElementById("game-result").innerText = "You lost the Game! Use the Restart button to restart!";
         }
     } else {
         console.log('game running');
@@ -98,13 +103,18 @@ function playGame(humanChoice, computerChoice) {
 }
 
 function handleRestart() {
-    // Resetting counters
+    // Resetting counters and elements to initial state
     humanScore = 0;
     computerScore = 0;
     round = 0;
-    document.getElementById("human-score").innerHTML = humanScore.toString();
-    document.getElementById("computer-score").innerHTML = computerScore.toString();
-    document.getElementById("round-counter").innerHTML = round.toString();
+    document.getElementById("human-score").innerText = humanScore.toString();
+    document.getElementById("computer-score").innerText = computerScore.toString();
+    document.getElementById("round-counter").innerText = (round + 1).toString();
+    document.getElementById("human-choice").innerText = "Your choice";
+    document.getElementById("computer-choice").innerText = "Computer's choice";
+    document.getElementById("round-result").innerText = "";
+    document.getElementById("game-result").innerText = "";
+
 
     // Resetting buttons
     let rockButton = document.getElementById('rock-button');
